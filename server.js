2db17e991175ae6eab282adcb46c8194a1f4bdad
mutasem <mutasem.halawani@gmail.com>
2018-05-05 10:50:02 +0300

    check for item if exists in DB

--- server.js
+++ server.js
@@ -10,23 +10,40 @@ var mongoDB = 'mongodb://mutasem:mutasem@ds111410.mlab.com:11410/testdb0'
 app.use(express.static(path.join(__dirname, 'public')))
 app.get('/new/:website', (req, res) => {
   const pattern = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/gm
+  var newWebsite = Math.floor(1000 + Math.random() * 9000)
+  // console.log(newWebsite)
   // console.log(req.params.website)
   let website = req.params.website
   let isValid = Boolean(pattern.test(website))
-  
+
   if (isValid) {
     mongoose.connect(mongoDB, (err, db) => {
       if (err) { return err }
-      db.collection('websites').find({'old': website}).forEach((item) => {
-        console.log(item)
-        // res.send(JSON.stringify({
-        //   'short': item.new
-        // }))
+      // db.collection('websites').insert({
+      //   'old': website,
+      //   'new': newWebsite
+      // })
+
+      db.collection('websites').findOne({'old': website}, function (err, item) {
+        if (err) {
+          console.log(err)
+          // return err
+        }
+        if (item) {
+          console.log('item: ', item)
+          // item.forEach((item) => {
+          //   res.send(JSON.stringify(item))
+          //   console.log(item)
+          // })
+        } else {
+          console.log('item doesnt exist')
+        }
       })
 
-      // db.close()
+      // db.die()
     })
   } else {
+    console.log('Error')
     res.send(JSON.stringify({
       'error': 'This url is invalid'
     }))
@@ -34,7 +51,7 @@ app.get('/new/:website', (req, res) => {
 })
 
 // app.get('/new/:website', (req, res) => {
-//     
+//
 //     let userInput = req.params.website;
 
 //     let isValid = Boolean(pattern.test(userInput));
