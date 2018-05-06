590622a6f395fd0fb7d338233fb40999a18d81b4
mutasem <mutasem.halawani@gmail.com>
2018-05-06 11:01:08 +0300

    edit adding new website and return the ones which already exist

--- server.js
+++ server.js
@@ -11,37 +11,33 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.get('/new/:website', (req, res) => {
   const pattern = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/gm
   var newWebsite = Math.floor(1000 + Math.random() * 9000)
-
-  // console.log(newWebsite)
-  // console.log(req.params.website)
   let website = req.params.website
   let isValid = Boolean(pattern.test(website))
 
   if (isValid) {
     mongoose.connect(mongoDB, (err, db) => {
       if (err) { return err }
-      // db.collection('websites').insert({
-      //   'old': website,
-      //   'new': newWebsite
-      // })
-
       db.collection('websites').findOne({'old': website}, (err, item) => {
-        if (err) {
-          console.log(err)
-          // return err
-        }
+        if (err) { return err }
         if (item) {
-          console.log('item: ', item)
-          // item.forEach((item) => {
-          //   res.send(JSON.stringify(item))
-          //   console.log(item)
-          // })
-        } else { // if doesnt exist add it to database
-          console.log('item doesnt exist')
+          console.log(typeof (item))
+          res.setHeader('Content-Type', 'application/json')
+          res.send(JSON.stringify({
+            'item exists before': 'true',
+            item
+          }))
+        } else {
+          db.collection('websites').insert({
+            'old': website,
+            'new': newWebsite
+          })
+          res.setHeader('Content-Type', 'application/json')
+          res.send(JSON.stringify({
+            'old': website,
+            'new': newWebsite
+          }))
         }
       })
-
-      db.close()
     })
   } else {
     res.setHeader('Content-Type', 'application/json')
@@ -52,28 +48,22 @@ app.get('/new/:website', (req, res) => {
 })
 
 app.get('/:short', (req, res) => {
-
   const numberPattern = /^[0-9]*$/
   let shortURL = Number(req.params.short)
   let isNumber = Boolean(numberPattern.test(shortURL))
-
-  // console.log(typeof (shortURL))
   if (isNumber) {
     mongoose.connect(mongoDB, (err, db) => {
-      if (err) {
-        console.log(err)
-        return err
-      }
+      if (err) { return err }
       db.collection('websites').findOne({'new': shortURL}, (err, item) => {
-        if (err) {
-          console.log(err)
-          return err
-        }
+        if (err) { return err }
         if (item) {
           let redirectURL = item.old
           res.redirect(`http://${redirectURL}`)
         } else {
-          console.log('item doesnt exist')
+          res.setHeader('Content-Type', 'application/json')
+          res.send(JSON.stringify({
+            'error': 'Item doesn\'t exist'
+          }))
         }
       })
     })
@@ -84,30 +74,5 @@ app.get('/:short', (req, res) => {
     }))
   }
 })
-
-// app.get('/new/:website', (req, res) => {
-//
-//     let userInput = req.params.website;
-
-//     let isValid = Boolean(pattern.test(userInput));
-//     console.log(isValid);
-//     if (isValid) {
-//         mongo.connect(url, (err, db) => {
-//             if (err) throw err;
-//             const docs = db.db('heroku_prtr1p3x').collection('urls');
-//             console.log('docs: ', docs);
-//             // docs.getAll().then(data => {
-//             //     console.log('New age: 40');
-//             // }).catch(console.log());
-
-//             db.close();
-//         });
-
-//     } else {
-//         res.send(JSON.stringify({
-//             "error": "This url is not on the database."
-//         }));
-//     }
-// });
 
 app.listen(port, console.log(`Listening on port ${port}`))
